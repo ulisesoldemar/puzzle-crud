7601a5b4faaf08cbbbd526686010dc8ae959e23a
Ulises Ortega <ulises.ortega2937@alumnos.udg.mx>
2022-03-19 19:28:40 -0600

    Modificados layouts de Breeze para ajustarse al CRUD

--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -1,67 +1,102 @@
 <script setup>
-import BreezeButton from '@/Components/Button.vue';
-import BreezeCheckbox from '@/Components/Checkbox.vue';
-import BreezeGuestLayout from '@/Layouts/Guest.vue';
-import BreezeInput from '@/Components/Input.vue';
-import BreezeLabel from '@/Components/Label.vue';
-import BreezeValidationErrors from '@/Components/ValidationErrors.vue';
-import { Head, Link, useForm } from '@inertiajs/inertia-vue3';
+import BreezeButton from "@/Components/Button.vue";
+import BreezeCheckbox from "@/Components/Checkbox.vue";
+import BreezeGuestLayout from "@/Layouts/Guest.vue";
+import BreezeInput from "@/Components/Input.vue";
+import BreezeLabel from "@/Components/Label.vue";
+import BreezeValidationErrors from "@/Components/ValidationErrors.vue";
+import { Head, Link, useForm } from "@inertiajs/inertia-vue3";
 
 defineProps({
-    canResetPassword: Boolean,
-    status: String,
+  canResetPassword: Boolean,
+  canRegister: Boolean,
+  status: String,
 });
 
 const form = useForm({
-    email: '',
-    password: '',
-    remember: false
+  email: "",
+  password: "",
+  remember: false,
 });
 
 const submit = () => {
-    form.post(route('login'), {
-        onFinish: () => form.reset('password'),
-    });
+  form.post(route("login"), {
+    onFinish: () => form.reset("password"),
+  });
 };
 </script>
 
 <template>
-    <BreezeGuestLayout>
-        <Head title="Log in" />
+  <BreezeGuestLayout>
+    <Head title="Log in" />
 
-        <BreezeValidationErrors class="mb-4" />
+    <BreezeValidationErrors class="mb-4" />
 
-        <div v-if="status" class="mb-4 font-medium text-sm text-green-600">
-            {{ status }}
-        </div>
+    <div v-if="status" class="mb-4 font-medium text-sm text-green-600">
+      {{ status }}
+    </div>
 
-        <form @submit.prevent="submit">
-            <div>
-                <BreezeLabel for="email" value="Email" />
-                <BreezeInput id="email" type="email" class="mt-1 block w-full" v-model="form.email" required autofocus autocomplete="username" />
-            </div>
+    <form @submit.prevent="submit">
+      <div>
+        <BreezeLabel for="email" value="Email" />
+        <BreezeInput
+          id="email"
+          type="email"
+          class="mt-1 block w-full"
+          v-model="form.email"
+          required
+          autofocus
+          autocomplete="username"
+        />
+      </div>
 
-            <div class="mt-4">
-                <BreezeLabel for="password" value="Password" />
-                <BreezeInput id="password" type="password" class="mt-1 block w-full" v-model="form.password" required autocomplete="current-password" />
-            </div>
+      <div class="mt-4">
+        <BreezeLabel for="password" value="Password" />
+        <BreezeInput
+          id="password"
+          type="password"
+          class="mt-1 block w-full"
+          v-model="form.password"
+          required
+          autocomplete="current-password"
+        />
+      </div>
 
-            <div class="block mt-4">
-                <label class="flex items-center">
-                    <BreezeCheckbox name="remember" v-model:checked="form.remember" />
-                    <span class="ml-2 text-sm text-gray-600">Remember me</span>
-                </label>
-            </div>
+      <div class="block mt-4">
+        <label class="flex items-center justify-between">
+          <div>
+            <BreezeCheckbox name="remember" v-model:checked="form.remember" />
+            <span class="ml-2 text-sm text-gray-600">Remember me</span>
+          </div>
+          <Link
+            v-if="canResetPassword"
+            :href="route('password.request')"
+            class="underline text-sm text-gray-600 hover:text-gray-900"
+          >
+            Forgot your password?
+          </Link>
+        </label>
+      </div>
 
-            <div class="flex items-center justify-end mt-4">
-                <Link v-if="canResetPassword" :href="route('password.request')" class="underline text-sm text-gray-600 hover:text-gray-900">
-                    Forgot your password?
-                </Link>
-
-                <BreezeButton class="ml-4" :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
-                    Log in
-                </BreezeButton>
-            </div>
-        </form>
-    </BreezeGuestLayout>
+      <div class="flex items-center justify-start mt-4">
+        <BreezeButton
+          class="ml-4"
+          :class="{ 'opacity-25': form.processing }"
+          :disabled="form.processing"
+        >
+          Log in
+        </BreezeButton>
+      </div>
+      <p class="text-sm font-semibold mt-2 pt-1 mb-0">
+        Don't have an account?
+        <Link
+          v-if="canRegister"
+          :href="route('register')"
+          class="underline text-sm text-gray-600 hover:text-gray-900"
+        >
+          Register
+        </Link>
+      </p>
+    </form>
+  </BreezeGuestLayout>
 </template>
--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -1,26 +1,132 @@
 <script setup>
-import BreezeAuthenticatedLayout from '@/Layouts/Authenticated.vue';
-import { Head } from '@inertiajs/inertia-vue3';
+import BreezeAuthenticatedLayout from "@/Layouts/Authenticated.vue";
+import { Head } from "@inertiajs/inertia-vue3";
 </script>
 
 <template>
-    <Head title="Dashboard" />
+  <Head title="Dashboard" />
 
-    <BreezeAuthenticatedLayout>
-        <template #header>
-            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-                Dashboard
-            </h2>
-        </template>
+  <BreezeAuthenticatedLayout>
+    <template #header>
+      <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+        Dashboard
+      </h2>
+    </template>
 
-        <div class="py-12">
-            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                    <div class="p-6 bg-white border-b border-gray-200">
-                        You're logged in!
-                    </div>
+    <div class="py-12">
+      <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+        <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+          <!-- Container for demo purpose -->
+          <div class="container my-6 px-6 mx-auto">
+            <!-- Section: Design Block -->
+            <section class="mb-32 text-gray-800 text-center md:text-left">
+              <h2 class="text-3xl font-bold mb-12 text-center">
+                Latest puzzles added
+              </h2>
+
+              <div
+                v-for="puzzle in puzzles"
+                :key="puzzle.id"
+                class="flex flex-wrap mb-6"
+              >
+                <div
+                  class="
+                    grow-0
+                    shrink-0
+                    basis-auto
+                    w-full
+                    md:w-3/12
+                    px-3
+                    mb-6
+                    md:mb-0
+                    ml-auto
+                  "
+                >
+                  <div
+                    class="
+                      relative
+                      overflow-hidden
+                      bg-no-repeat bg-cover
+                      relative
+                      overflow-hidden
+                      bg-no-repeat bg-cover
+                      ripple
+                      shadow-lg
+                      rounded-lg
+                      mb-6
+                    "
+                    data-mdb-ripple="true"
+                    data-mdb-ripple-color="light"
+                  >
+                    <img
+                      :src="showImage() + puzzle.image"
+                      class="w-min"
+                      alt="Louvre"
+                    />
+                    <a :href="route('puzzles.show', puzzle.id)">
+                      <div
+                        class="
+                          absolute
+                          top-0
+                          right-0
+                          bottom-0
+                          left-0
+                          w-full
+                          h-full
+                          overflow-hidden
+                          bg-fixed
+                          opacity-0
+                          hover:opacity-100
+                          transition
+                          duration-300
+                          ease-in-out
+                        "
+                        style="background-color: rgba(251, 251, 251, 0.2)"
+                      ></div>
+                    </a>
+                  </div>
                 </div>
-            </div>
+                <div
+                  class="
+                    grow-0
+                    shrink-0
+                    basis-auto
+                    w-full
+                    md:w-9/12
+                    xl:w-7/12
+                    px-3
+                    mb-6
+                    md:mb-0
+                    mr-auto
+                  "
+                >
+                  <h5 class="text-lg font-bold mb-3">{{ puzzle.name }}</h5>
+                  <p class="text-gray-500 mb-6">
+                    <small>Updated at <u>{{ new Date(puzzle.updated_at) }}</u></small>
+                  </p>
+                </div>
+              </div>
+            </section>
+            <!-- Section: Design Block -->
+          </div>
+          <!-- Container for demo purpose -->
         </div>
-    </BreezeAuthenticatedLayout>
+      </div>
+    </div>
+  </BreezeAuthenticatedLayout>
 </template>
+
+<script>
+export default {
+  methods: {
+    showImage() {
+      return "/storage/";
+    },
+  },
+
+  props: {
+    puzzles: Array,
+    count: Number,
+  },
+};
+</script>
